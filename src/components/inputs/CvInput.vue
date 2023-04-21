3b0bd76106e717058f263eeeb43e922e16a6037c
Ivan Kordonets <ivan.kordonets@virtuozzo.com>
2023-04-21 23:46:34 +0300

    add edit icon for cv input

--- src/components/inputs/CvInput.vue
+++ src/components/inputs/CvInput.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, withDefaults, ref, nextTick, computed} from 'vue';
+import { defineProps, withDefaults, ref, nextTick, computed, onMounted} from 'vue';
 import { useUserData } from "@/helpers/user";
 
 const { fillToken, fillConfig, fillMainCvId, fillMainCv, updateUser } = useUserData();
@@ -28,10 +28,31 @@ const isPhone = ref(false);
 const isShowLabel = ref(true);
 const isShowInput = ref(false);
 const inputValue = ref('');
-
 inputValue.value = props.previousValue;
 
+const inputWidth = ref(inputValue.value.length);
+
+const cssVars = computed(() => {
+    return {'width': (inputWidth.value -1) * 1 + 'ch'};
+});
+
+onMounted(() => {
+  if (myInput.value) {
+    myInput.value.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        myInput.value.blur();
+      }
+    });
+  }
+  onSubmit();
+});
+
 const handleInput = (arg) => {
+
+    inputWidth.value = inputValue.value.length;
+
+    // console.log('typing', inputValue.value, inputWidth.value, cssVars);
+
     if (arg === 'click') {
         isShowLabel.value = false;
         isShowInput.value = true;
@@ -42,6 +63,12 @@ const handleInput = (arg) => {
     }
 
     if (arg === 'blur' && inputValue.value.length < 1) {
+        console.log('blur');
+
+        nextTick(() => {
+            myInput.value.unfocus();
+         });
+
         isShowInput.value = false;
         isShowLabel.value = true;
     }
@@ -58,17 +85,22 @@ const handleInput = (arg) => {
 
 handleInput(inputValue.value);
 
-const onSubmit = () => {
+const onSubmit = () => {    
+    console.log('submit');
     updateUser(props.param, inputValue.value);
 }
 
 </script>
 
 <template>
-    <div class="inputCv" @click="handleInput('click')">
+    <div 
+        class="inputCv" 
+        @click="handleInput('click')" 
+    >
         <label v-if="isShowLabel" class="label"> {{ label }} </label>
         <input 
             ref="myInput"
+            :style="cssVars"
             v-if="isShowInput" 
             v-model="inputValue"
             :value='inputValue'
@@ -78,14 +110,30 @@ const onSubmit = () => {
             :placeholder=placeholder 
             @blur="handleInput('blur')"
             @input="handleInput('input')"
-            @keyup.enter="onSubmit(label)"
+            @keyup.enter="onSubmit()"
         >
+        <svg class="editIcon" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.7813 2.86274C11.1404 2.50377 11.6274 2.30215 12.1351 2.30225C12.6429 2.30234 13.1298 2.50413 13.4888 2.86324C13.8478 3.22234 14.0494 3.70934 14.0493 4.21709C14.0492 4.72485 13.8474 5.21177 13.4883 5.57074L12.7553 6.30274L10.0463 3.59674L10.7803 2.86274H10.7813ZM9.34128 4.30374L3.38528 10.2577C3.2045 10.4387 3.06824 10.6591 2.98728 10.9017L2.07328 13.6447C2.04408 13.7328 2.03994 13.8272 2.06131 13.9174C2.08268 14.0076 2.12873 14.0901 2.1943 14.1557C2.25987 14.2213 2.34239 14.2673 2.43263 14.2887C2.52286 14.3101 2.61727 14.3059 2.70528 14.2767L5.44828 13.3627C5.69128 13.2827 5.91128 13.1457 6.09228 12.9647L12.0483 7.01074L9.34028 4.30374H9.34128Z" fill="#B3B3B3"/>
+        </svg>
     </div>
 </template>
 
 <style scoped lang="scss">
-    .inputCv {
 
+    .inputCv {
+        display: flex;
+        margin-top: 3px;
+        
+        input {
+            cursor: pointer;
+        }
+
+        &:hover .editIcon{
+            display: block;
+        }    
+    }
+    .editIcon{
+        display: none;
     }
 
     .label {
@@ -111,7 +159,7 @@ const onSubmit = () => {
         appearance: none;
         font-size: 13px;
         outline: none;
-        width: 150px;
+        // width: 150px;
     }
 
     input[type=text]:focus, input[type=tel]:focus, input[type=url]:focus {
